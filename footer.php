a6980b561ab19b99d9f8541fb32693de9e591c6a
BeauWilliams97 <beau.williams@my.jcu.edu.au>
2021-05-18 14:09:32 +1000

    changed the div it was wrapped in as the previous changes made to .site-content caused the footer to break

--- footer.php
+++ footer.php
@@ -8,24 +8,31 @@
  *
  * @package joemama
  */
-
 ?>
 
-<footer id="colophon" class="site-footer">
-	<div class="site-info">
-		<?php
-		printf( 'Townsville Jazz Club 2021 ©');
-		?>
-		<span class="sep"> | </span>
-		<?php
-		printf('Privacy Policy');
-		?>
-		<span class="sep"> | </span>
-		<?php
-		printf( 'Terms of Use');
-		?>
-	</div><!-- .site-info -->
-</footer><!-- #colophon -->
+	</div><!-- #content -->
+
+	<?php get_sidebar( 'footer' ); ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-footer__wrap">
+            <?php
+            printf( 'Townsville Jazz Club 2021 ©');
+            ?>
+            <span class="sep"> | </span>
+            <?php
+            printf('Privacy Policy');
+            ?>
+            <span class="sep"> | </span>
+            <?php
+            printf( 'Terms of Use');
+            ?>
+			<div class="site-info">
+				<div><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'team12theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'Team-git 12' ), 'WordPress' ); ?></a></div>
+				<div><?php printf( esc_html__( 'Team-12 theme' ), 'team12theme', '<a href="https://github.com/BeauWilliams97/cp3402-2021-site-cp3402-project-team12.git" rel="designer">CP3402 team12</a>' ); ?></div>
+			</div><!-- .site-info -->
+		</div><!-- .site-footer__wrap -->
+	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
